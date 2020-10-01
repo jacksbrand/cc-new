a6b6e00ca3deb8cc6a858cde214001a9abda8f77
Jack <jbizzell@protonmail.com>
2020-10-01 18:47:26 +0100

    setting redux loggers to only run in development

--- src/redux/store.js
+++ src/redux/store.js
@@ -9,7 +9,11 @@ import {
 } from 'redux-persist'
 
 
-const middlewares = [logger];
+const middlewares = [];
+
+if (process.env.NODE_ENV === 'development') {
+  middlewares.push(logger)
+}
 
 const store = createStore(rootReducer, applyMiddleware(...middlewares))
 
