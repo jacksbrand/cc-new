5c9ccefb210303f675b7425563f0f957f7bfb717
Jack <jack@pop-os.localdomain>
2020-10-12 13:32:51 +0100

    adding express server for payments

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
@@ -7,10 +8,25 @@ const StripeCheckoutButton = ({ price }) => {
     'pk_test_51HUbNPCh4Uw4gYgzxKrMD8hJQjOYDVVg5Qjgf4xNWpfgnWajhCFg2GIWiMWMtnuX20bmS1EZ2rvpnrh7odPiKnCc00C5xSpHme';
 
   const onToken = (token) => {
-    console.log(token);
-    alert(
-      '(TEST) Payment Successful - cart left full for convenience and futher testing'
-    );
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token,
+      },
+    })
+      .then((response) => {
+        alert(
+          '(TEST) Payment Successful - cart left full for convenience and futher payment testing'
+        );
+      })
+      .catch((error) => {
+        console.log('Payment error: ', error);
+        alert(
+          'Payment process encountered an issue. Please use the provided test credit card details, which can be found on the checkout page, above the payment button.'
+        );
+      });
   };
 
   return (
@@ -21,7 +37,7 @@ const StripeCheckoutButton = ({ price }) => {
       billingAddress
       shippingAddress
       image="https://svgshare.com/i/CUz.svg"
-      description={`Your total is: £${price}`}
+      description={`Total: £${price}`}
       amount={priceForStripe}
       panelLabel="Pay Now"
       token={onToken}
