d3d91e6c17a3c0b5259477091f696871ebedbd4b
Jack <jack@pop-os.localdomain>
2020-10-12 13:39:57 +0100

    updating checkout page default test payment message

--- client/src/pages/checkout/checkout.component.jsx
+++ client/src/pages/checkout/checkout.component.jsx
@@ -13,34 +13,36 @@ import StripeCheckoutButton from '../../components/stripe-button/stripe-button.c
 
 const CheckoutPage = ({ cartItems, total }) => {
   return (
-    <div className='checkout-page'>
-      <div className='checkout-header'>
-        <div className='header-block'>
+    <div className="checkout-page">
+      <div className="checkout-header">
+        <div className="header-block">
           <span>Product</span>
         </div>
-        <div className='header-block'>
+        <div className="header-block">
           <span>Description</span>
         </div>
-        <div className='header-block'>
+        <div className="header-block">
           <span>Quantity</span>
         </div>
-        <div className='header-block'>
+        <div className="header-block">
           <span>Price</span>
         </div>
-        <div className='header-block'>
+        <div className="header-block">
           <span>Remove</span>
         </div>
       </div>
       {cartItems.map((cartItem) => (
         <CheckoutItem key={cartItem.id} cartItem={cartItem} />
       ))}
-      <div className='total'>
+      <div className="total">
         <span>TOTAL: £{total}</span>
       </div>
-      <div className='test-warning'>
+      <div className="test-warning">
         * Please use the following test credit card for payments:
         <br />
-        4242 4242 4242 4242 - 01/22 - 123 *
+        4242 4242 4242 4242 - 01/22 - 123
+        <br />
+        and use 'made-up' information for address *
       </div>
 
       <StripeCheckoutButton price={total} />
